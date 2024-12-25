0b855a6ac0b29aa3016119bb1d81b199658d113d
mrothauer <office@rothauer-it.com>
2024-12-25 20:57:34 +0100

    checkletters

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -2,24 +2,65 @@ class Hangman {
 
     constructor() {
         this.words = [];
+        this.word = '';
+        this.typedLetters = [];
+        this.setWords(words);
+        this.setWord();
+        this.bindLetterEvents();
+    }
+
+    bindLetterEvents() {
+        const letters = 'abcdefghijklmnopqrstuvwxyzäöüß';
+        letters.split('').forEach(letter => {
+            document.addEventListener('keypress', (event) => {
+                if (event.key.toLowerCase() === letter) {
+                    this.typedLetters.push(letter);
+                    this.typedLetters = [...new Set(this.typedLetters)];
+                    this.checkTypedLetters();
+                }
+            });
+        });
     }
 
     setWords(words) {
         this.words = words;
     }
 
-    getRandWord() {
-        return this.words[Math.floor(Math.random() * this.words.length)];
+    setWord() {
+        this.word = this.words[Math.floor(Math.random() * this.words.length)];
+    }
+
+    getWord() {
+        return this.word;
+    }
+
+    checkTypedLetters() {
+        const wordOutput = this.word.split('').map(letter => {
+            if (this.typedLetters.includes(letter.toLocaleLowerCase())) {
+                return letter;
+            }
+            return '_';
+        }).join(' ');
+
+        const wordContainer = document.getElementById('word');
+        wordContainer.innerHTML = wordOutput;
+    }
+
+    getWordOutput() {
+        return this.word.split('').map(letter => '_').join(' ');
+    }
+
+    render() {
+        const wordContainer = document.getElementById('word');
+        wordContainer.innerHTML = this.getWordOutput();
     }
 
 }
 
 const hangman = new Hangman();
-hangman.setWords(words);
-const word = hangman.getRandWord();
+const word = hangman.getWord();
 
 // render the word to output
-const output = document.getElementById('output');
+console.log(word);
+hangman.render();
 
-// output the length of the word
-output.innerHTML = word.split('').map(letter => '_').join(' ');
