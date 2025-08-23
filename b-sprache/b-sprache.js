1997fb3e27a138da49d648ec40c94d2e19f27f01
mrothauer <office@rothauer-it.com>
2025-08-23 22:48:47 +0200

    b-sprache

---
+++ b-sprache/b-sprache.js
@@ -0,0 +1,42 @@
+document.getElementById('inputText').addEventListener('keyup', function() {
+    translate(this.value);
+});
+
+function translate(inputText) {
+
+    const mainLetter = 'b';
+
+    let translatedText = '';
+    let currentChar = '';
+    let nextChar = '';
+    let secondVowel = '';
+
+    for (let i = 0; i < inputText.length; i++) {
+        currentChar = inputText[i];
+        nextChar = inputText[i + 1] ?? '';
+
+        translatedText += currentChar;
+        if (isVowel(currentChar.toLowerCase())) {
+            if (isVowel(nextChar.toLowerCase()) && isValidDoubleVowel(currentChar.toLowerCase(), nextChar.toLowerCase())) {
+                secondVowel = currentChar.toLowerCase();
+            } else {
+                translatedText += mainLetter + secondVowel + currentChar.toLowerCase();
+                secondVowel = '';
+            }
+        }
+
+    }
+
+    let output = translatedText;
+
+    let outputContainer = document.getElementById('output');
+    outputContainer.textContent = output;
+}
+
+function isValidDoubleVowel(char1, char2) {
+    return ['ei', 'ie', 'aa', 'ee', 'oo', 'au', 'eu', 'äu', 'ai'].includes(char1 + char2);
+}
+
+function isVowel(char) {
+    return ['a', 'e', 'i', 'o', 'u', 'ä', 'ö', 'ü'].includes(char);
+}
